777afb9948bd517cf7796aa8635db001c5a4b6e6
vipin <vipinvishwa1m1@gmail.com>
2023-04-17 13:30:09 +0530

    Removed circle marking from the map and replaced the for loop with the JS mapping method to iterate over the data.

--- maps/index.js
+++ maps/index.js
@@ -1,53 +1,59 @@
-// Fetch the CSV file using Fetch API
-fetch('data.csv')
-  .then(response => response.text())
-  .then(data => {
-    // Parse the CSV data
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-    const jsonData = [];
+// Initialize the map and set its view to our chosen geographical coordinates and a zoom level:
+var map = L.map("map").setView([30.900965, 75.857277], 12);
 
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i].split(',');
-      const rowData = {};
-      for (let j = 0; j < headers.length; j++) {
-        rowData[headers[j]] = row[j];
-      }
-      jsonData.push(rowData);
-    }
+// tileLayer: Used to load and display tile layers on the map, in this case it’s a OpenStreetMap tile layer.
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
-    // Create Leaflet map
-    const map = L.map('map').setView([30.91, 75.86], 11); // Set initial map view
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Add OpenStreetMap tile layer
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+// NOTE: Make sure all the code is called after the div and leaflet.js inclusion.
 
-    // Define custom icon
-    const customIcon = L.icon({
-      iconUrl: 'https://gndec.ac.in/~hsrai/maps/coe/coe2.jpeg', // Specify the URL of the custom icon image
-      iconSize: [20, 20], // Specify the size of the icon
-      iconAnchor: [16, 16] // Specify the anchor point of the icon
+// POPUP Lat and Lng on click on anywhere on map
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on("click", onMapClick);
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+fetch("./data.csv")
+  .then((response) => response.text())
+  .then((data) => {
+    const dataArray = data.split("\n").map((row) => row.split(","));
+    const [headings, ...others] = dataArray;
+
+    const coeDataArray = others.map((item) => ({
+      lat: parseFloat(item[0]),
+      lon: parseFloat(item[1]),
+      title: item[2],
+      description: item[3],
+      icon: item[4],
+      iconSize: item[5].split(",").map((iItem) => parseFloat(iItem)),
+      iconOffset: item[6].split(",").map((iItem) => parseFloat(iItem)),
+    }));
+
+    coeDataArray.map((data_item) => {
+      var myIcon = L.icon({
+        iconUrl: `./${data_item.icon}`,
+        iconSize: data_item.iconSize,
+        iconOffset: data_item.iconOffset,
+      });
+
+      var marker = L.marker([data_item.lat, data_item.lon], {
+        icon: myIcon,
+      })
+        .addTo(map)
+        .bindPopup(
+          `<b>${data_item.title}</b> <br/> <b>${data_item.description}</b>`
+        );
     });
-
-    // Loop through the data and add markers to the map with custom icon
-    for (let i = 0; i < jsonData.length; i++) {
-      // Check for valid latitude and longitude values
-      if (jsonData[i].lat !== undefined && jsonData[i].lon !== undefined && !isNaN(jsonData[i].lat) && !isNaN(jsonData[i].lon)) {
-        const lat = parseFloat(jsonData[i].lat); // Convert latitude string to number
-        const lon = parseFloat(jsonData[i].lon); // Convert longitude string to number
-        const marker = L.marker([lat, lon], {icon: customIcon}).addTo(map); // Add marker with custom icon
-        marker.bindPopup(`<b>${jsonData[i].title}</b><br>${jsonData[i].description}`); // Add popup with data
-
-      // Add circle with specified radius
-      
-      L.circle([lat, lon], {        
-      color: "red",
-      fillColor: "#f03",
-      fillOpacity: 0.5,
-      radius: 100,}).addTo(map);
-      }
-    }
   })
-  .catch(error => {
-    console.error('Error fetching CSV file:', error);
-  });
+  .catch((error) => console.error(error));
